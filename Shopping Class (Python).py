429d2f63ec52783cd675e507795abab444a09bd8
Grim4521 <james.stead12@gmail.com>
2022-02-19 21:27:34 +0000

    created an file

--- Shopping Class (Python).py
+++ Shopping Class (Python).py
@@ -1,1 +1,13 @@
-print ("Placeholder")
+list = []
+f = open ("Shoppinglist.txt", "x")
+print ("Welcome to the shopping list.")
+while True:
+    question = input ("Would you like to add anything (yes to add no to continue) ")
+    if question.lower() == "yes":
+        listadd = input ("What would you like to add ")
+        list.append(listadd)
+        f.write(listadd)
+    if question.lower() == "no":
+        break
+print ("In your list you have",list,)
+f.close()
